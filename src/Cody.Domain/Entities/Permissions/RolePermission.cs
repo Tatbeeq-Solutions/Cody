3e0796358f317b56200e7f91fc3f6a1087acf55f
Umidjon <makhammadsoliyev@gmail.com>
2024-05-06 01:25:15 +0500

    Add RolePermission And User Entities

---
+++ src/Cody.Domain/Entities/Permissions/RolePermission.cs
@@ -0,0 +1,10 @@
+﻿namespace Cody.Domain.Entities.Permissions;
+
+public class RolePermission
+{
+    public long RoleId { get; set; }
+    public long PermissionId { get; set; }
+
+    // public Role Role { get; set; }
+    // public Permission Permission { get; set; }
+}
