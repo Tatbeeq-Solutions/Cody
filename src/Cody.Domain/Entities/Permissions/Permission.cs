dd999c24eae5e96716c85393d15771bc68c1c467
Umidjon <makhammadsoliyev@gmail.com>
2024-05-06 01:14:13 +0500

    Add Role And Permissions Entities

---
+++ src/Cody.Domain/Entities/Permissions/Permission.cs
@@ -0,0 +1,9 @@
+﻿using Cody.Domain.Commons;
+
+namespace Cody.Domain.Entities.Permissions;
+
+public class Permission : Auditable
+{
+    public string Action { get; set; }
+    public string Controller { get; set; }
+}
