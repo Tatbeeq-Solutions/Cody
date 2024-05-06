cc98d177d53165931dc761cb4c5c5f6f5e0b50a5
Umidbek Maxammadsoliyev <149594973+makhammadsoliyev@users.noreply.github.com>
2024-05-07 01:11:00 +0500

    Revert "Add Role And Permissions Entities"

--- src/Cody.Domain/Entities/Permission.cs
+++
@@ -1,9 +0,0 @@
-﻿using Cody.Domain.Commons;
-
-namespace Cody.Domain.Entities;
-
-public class Permission : Auditable
-{
-    public string Action { get; set; }
-    public string Controller { get; set; }
-}
