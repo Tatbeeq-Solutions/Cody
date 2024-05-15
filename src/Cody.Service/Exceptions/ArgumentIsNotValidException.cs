4da33e13f641d6b00e0562c1ad310da915728620
Sayidahror Mirzaakhmedov <146230372+SaidahrorMirzaahmed@users.noreply.github.com>
2024-05-15 15:52:50 +0500

    Added Exceptions

---
+++ src/Cody.Service/Exceptions/ArgumentIsNotValidException.cs
@@ -0,0 +1,12 @@
+﻿namespace Cody.Service.Exceptions;
+
+public class ArgumentIsNotValidException : Exception
+{
+    public ArgumentIsNotValidException() { }
+
+    public ArgumentIsNotValidException(string message) : base(message) { }
+
+    public ArgumentIsNotValidException(string message, Exception exception) { }
+
+    public static int StatusCode => 400;
+}
---
+++ src/Cody.Service/Exceptions/NotFoundException.cs
@@ -0,0 +1,12 @@
+﻿namespace Cody.Service.Exceptions;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException() { }
+
+    public NotFoundException(string message) : base(message) { }
+
+    public NotFoundException(string message, Exception exception) { }
+
+    public static int StatusCode => 404;
+}
