16128f030a03a4b8176f6cde4f27e538ebc4f40d
Sayidahror Mirzaakhmedov <146230372+SaidahrorMirzaahmed@users.noreply.github.com>
2024-05-15 15:59:01 +0500

    PasswordHasher Added

---
+++ src/Cody.Service/Helpers/PasswordHasher.cs
@@ -0,0 +1,14 @@
+﻿namespace Cody.Service.Helpers;
+public static class PasswordHasher
+{
+    public static string Hash(string password)
+    {
+        var salt = BCrypt.Net.BCrypt.GenerateSalt(12);
+        return BCrypt.Net.BCrypt.HashPassword(password, salt);
+    }
+
+    public static bool Verify(string password, string passwordHash)
+    {
+        return BCrypt.Net.BCrypt.Verify(password, passwordHash);
+    }
+}
